ad14cf64ece27f331662c0d69bd4e4c91e6e1fdf
goatyeah <cazin.gaultier@gmail.com>
2015-07-01 16:00:09 +0200

    first commit

---
+++ src/Component/Component.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace Blank;
+
+class Components extends \Twig_Extension
+{
+
+  	public static function getName()
+    {
+        return 'twig_components';
+    }
+
+    public function getFunctions()
+    {
+		return array(
+            'component' => new \Twig_Function_Method($this, 'get_component', array('is_safe' => array('html'),'needs_environment' => true))
+        );
+    }
+
+    public function get_component(\Twig_Environment $twig, $name, $data)
+    {
+    	$html = $twig->render('components/'.$name.'.html.twig', $data );
+    	return $html;
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once __DIR__ . '/../../autoload.php'; // Autoload files using Composer autoload
+
+use Blank\Components;
+
+echo Components::getName();
