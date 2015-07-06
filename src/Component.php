c8d6f82a52da58b8328fc095b80e3f8be2025aad
goatyeah <cazin.gaultier@gmail.com>
2015-07-06 17:17:04 +0200

    autoload

--- src/Component.php
+++ src/Component.php
@@ -1,31 +1,11 @@
 <?php namespace Blank;
 
-use \Twig_Extension;
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/autoload.php'; // Autoload files using Composer autoload
 
 class Component extends \Twig_Extension
 {
 
-    // function __construct(){
-    //     spl_autoload_register(__NAMESPACE__ . "\\Component");
-    // }
-
-    // public static function autoload($className)
-    // {   
-    //     var_dump($className);
-    //     $className = ltrim($className, '\\');
-    //     $fileName  = '';
-    //     $namespace = '';
-    //     if ($lastNsPos = strrpos($className, '\\')) {
-    //         $namespace = substr($className, 0, $lastNsPos);
-    //         $className = substr($className, $lastNsPos + 1);
-    //         $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    //     }
-    //     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    //     require $fileName;
-    // }
-
-  	public static function getName()
+  	public function getName()
     {
         return 'twig_components';
     }
--- tests/test.php
+++ tests/test.php
@@ -1,6 +1,5 @@
 <?php 
 
 require dirname(dirname(dirname(dirname(__FILE__)))) . '/autoload.php'; // Autoload files using Composer autoload
-use Blank\Component;
 
-echo Blank\Component::getName();
+echo \Blank\Component::getName();
