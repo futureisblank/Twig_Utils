04ae24b5de3058c474c145b9cbf28fa1a87be6dd
goatyeah <cazin.gaultier@gmail.com>
2015-07-06 16:57:24 +0200

    autoload

--- src/Component.php
+++ src/Component.php
@@ -5,25 +5,25 @@ use \Twig_Extension;
 class Component extends \Twig_Extension
 {
 
-    function __construct(){
-        spl_autoload_register(__NAMESPACE__ . "\\Component");
-    }
+    // function __construct(){
+    //     spl_autoload_register(__NAMESPACE__ . "\\Component");
+    // }
 
-    public static function autoload($className)
-    {   
-        var_dump($className);
-        $className = ltrim($className, '\\');
-        $fileName  = '';
-        $namespace = '';
-        if ($lastNsPos = strrpos($className, '\\')) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-        }
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    // public static function autoload($className)
+    // {   
+    //     var_dump($className);
+    //     $className = ltrim($className, '\\');
+    //     $fileName  = '';
+    //     $namespace = '';
+    //     if ($lastNsPos = strrpos($className, '\\')) {
+    //         $namespace = substr($className, 0, $lastNsPos);
+    //         $className = substr($className, $lastNsPos + 1);
+    //         $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    //     }
+    //     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-        require $fileName;
-    }
+    //     require $fileName;
+    // }
 
   	public static function getName()
     {
--- tests/test.php
+++ tests/test.php
@@ -1,5 +1,6 @@
 <?php 
 
 require dirname(dirname(dirname(dirname(__FILE__)))) . '/autoload.php'; // Autoload files using Composer autoload
+use Blank\Component;
 
-echo \Blank\Component::getName();
+echo Blank\Component::getName();
